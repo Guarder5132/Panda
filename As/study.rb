3b47fcda58a3d6317236f4708da1eddb71c9555e
Guarder <Guarder@example.com>
2019-03-12 11:33:29 +0800

    完成用户的改 看 删 访问权限

--- As/study.rb
+++ As/study.rb
@@ -1,3 +1,5 @@
-3.30fen
+3.30fen  
+
+1小时  看 删 改 权限
 
 <% @user.errors.full_messages.each do |error| %>
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,8 @@
 class UsersController < ApplicationController
+  before_action :signed_in_user, only: [:edit,:update,:index,:destroy]
+  before_action :correct_user, only: [:edit, :update]
+  before_action :admin_user, only: :destroy
+
   def new
     @user = User.new
   end
@@ -17,6 +21,24 @@ class UsersController < ApplicationController
   def edit
   end
 
+  def update
+    if @user.update_attributes(user_params)
+      flash[:success] = "Update Success"
+      redirect_to @user
+    else
+      render 'edit'
+    end
+  end
+
+  def index
+    @users = User.paginate(page: params[:page])
+  end
+
+  def destroy
+    User.find(params[:id]).destroy
+    redirect_to users_path
+  end
+
   def show
     @user = User.find(params[:id])
   end
@@ -25,5 +47,18 @@ class UsersController < ApplicationController
 
     def user_params
       params.require(:user).permit(:name,:email,:password,:password_confirmation)
+    end
+
+    def admin_user 
+      redirect_to root_path unless current_user.admin?
+    end
+
+    def signed_in_user
+      redirect_to signin_path , notice: "please signin"  unless signed_in?
+    end
+
+    def correct_user 
+      @user = User.find(params[:id])
+      redirect_to root_path, notice: "no no no" unless current_user?(@user)
     end
 end
